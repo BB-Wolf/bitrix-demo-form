088938d713565c08ab9e74a7b6985e8668352e96
Kirsanicate <arli-chan@yandex.ru>
2023-03-31 18:08:55 +0300

    Вывод пагинации, передача данных из класса.

--- www/local/components/bitrix/demo_list/class.php
+++ www/local/components/bitrix/demo_list/class.php
@@ -8,8 +8,15 @@ use Bitrix\Highloadblock\HighloadBlockTable;
 class DemoList extends \CBitrixComponent 
 {
 
+    // Для сортировки можно задать отдельные сеттер\гететр которые будут задаваться через ajax-controller, но мы пойдем простым путем и будем брать из GET, конечно лучше использовать
+    // методы встроенные и не доверять пользовательскому вводу.
+
+    private $nav;
+
+    
    
-    public function getData(){
+    public function getData(): Array    {
+
         $hlBlock = HighloadBlockTable::getById(FEEDBACK_HLBLOCK_ID)->fetch();
         $hlBlockEntity = HighloadBlockTable::compileEntity($hlBlock);
         $entityDataClass = $hlBlockEntity->getDataClass();
@@ -17,13 +24,34 @@ class DemoList extends \CBitrixComponent
 
         $nav = new \Bitrix\Main\UI\PageNavigation("nav-more-notice");
         $nav->allowAllRecords(true)
-        ->setPageSize(2)
+        ->setPageSize(4)
         ->initFromUri();
 
 
+        $this->nav = $nav;
+
+        $resultData = $entityDataClass::getList(array(
+            'select' => array('*'),
+            "count_total" => true,
+            "offset" => $nav->getOffset(), 
+            "limit" => $nav->getLimit(),
+            'order'=>['ID'=>strip_tags(trim($_GET['sort']))?:'ASC'],
+        ));
+
+        $nav->setRecordCount($resultData->getCount());
+
+        $arResult = $resultData->fetchAll();
+        return $arResult;
     }
 
 
-    public function executeComponent(){}
+    public function executeComponent(){
+
+
+        $this->arResult['ITEMS']=$this->getData();
+        $this->arResult['NAV'] =  $this->nav;
+        $this->includeComponentTemplate();
+        
+    }
 
 }
--- www/local/components/bitrix/demo_list/templates/.default/template.php
+++ www/local/components/bitrix/demo_list/templates/.default/template.php
@@ -3,7 +3,7 @@
 use Bitrix\Main\Loader;
 use Bitrix\Highloadblock\HighloadBlockTable;
 
-CJSCore::Init(); 
+CJSCore::Init();
 
 if (!Loader::IncludeModule('highloadblock')) {
     ShowError('Не найден модуль Highloadblock');  // здеь лучше использовать языковые переменные: GetMessage('') но думаю, что в рамках теста - простителен хардкод.
@@ -13,33 +13,54 @@ if (!Loader::IncludeModule('highloadblock')) {
 $hlBlock = HighloadBlockTable::getById(FEEDBACK_HLBLOCK_ID)->fetch();
 $hlBlockEntity = HighloadBlockTable::compileEntity($hlBlock);
 $entityDataClass = $hlBlockEntity->getDataClass();
-    // В целом, тут можно обойтись без этих вызовов. Используя прямые запросы к БД, но мы так делать не будем :)
+// В целом, тут можно обойтись без этих вызовов. Используя прямые запросы к БД, но мы так делать не будем :)
 
-$nav = new \Bitrix\Main\UI\PageNavigation("nav-more-notice");
-$nav->allowAllRecords(true)
-      ->setPageSize(2)
-    ->initFromUri();
 ?>
 
+<style>
+    .feedback {
+        margin-top: 20px;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+
+    .feedback__list ul {
+        margin-top: 30px !important;
+        
+    }
+
+    .feedback__list ul li,#workarea li {
+        text-indent: unset !important;
+    }
+</style>
 
 <section class="feedback">
-    <?
-     $arResult =  $entityDataClass::getList([
-        'select'=>[
-            '*'
-        ],
-        'order'=>[
-            'ID'=>'ASC'
-        ],
-
-     ]);
-
-    ?>
     <div class="feedback__list">
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
+        <?
+        if ($arResult['ITEMS']) :
+            foreach ($arResult['ITEMS'] as $arItem) :
+                if ($arItem['UF_FIO'] == '') {
+                    continue;
+                } ?>
+                <ul>
+                    <li>ФИО: <?= $arItem['UF_FIO'] ?></li>
+                    <li>E-mail:<? echo substr($arItem['UF_EMAIL'], 0, strpos($arItem['UF_EMAIL'], '@')); ?></li>
+                    <li>Сообщение: <?= $arItem['UF_FIO']; ?></li>
+
+                </ul>
+        <? endforeach;
+        endif; ?>
     </div>
 </section>
+
+<?
+$APPLICATION->IncludeComponent(
+    "bitrix:main.pagenavigation",
+    "",
+    array(
+        "NAV_OBJECT" => $arResult['NAV'],
+        "SEF_MODE" => "N"
+    ),
+    false
+);
